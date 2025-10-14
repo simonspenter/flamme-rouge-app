d267da55a7acce145eb423423ec078efad314d9d
simonspenter <simonspenter@gmail.com>
2025-10-14 12:47:52 +0200

    manglede lige en fil

---
+++ constants/icons.py
@@ -0,0 +1,8 @@
+# constants/icons.py
+stage_type_icons = {
+    "Cobble stone": "cobblestone.svg",
+    "Flat": "flat.svg",
+    "Hilly": "hilly.svg",
+    "Mountain": "mountain.svg",
+    "Time trial": "timetrial.svg",
+}
